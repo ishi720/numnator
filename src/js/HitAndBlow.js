3a9d5e4769c548a138a2d387ef81adf75acffdb1
ishi720 <ishizuka720@gmail.com>
2021-09-18 11:35:23 +0900

    HitAndBlow.jsを追加

---
+++ src/js/HitAndBlow.js
@@ -0,0 +1,23 @@
+'use strict';
+
+export class HitAndBlow {
+  constructor () {
+    this.answer = [1,2,3];
+    this.count = 0;
+  }
+
+  question(arr) {
+    var hitCnt = 0;
+    var blowCnt = 0;
+    var _this = this;
+    this.answer.forEach(function(v,i){
+      if (_this.answer[1] === arr[i]) {
+        hitCnt++;
+      }
+      if (_this.answer.includes(arr[i])) {
+        blowCnt++;
+      }
+    });
+    return [hitCnt,blowCnt-hitCnt];
+  }
+}
